05444cce24d1fa50ead66c410e049d7d324b2a8c
thiti012 <91051080+thiti012@users.noreply.github.com>
2025-04-19 16:59:23 +0700

    signin

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,8 +1,45 @@
 'use client';
 
+import axios from "axios";
 import React from "react";
+import {useState } from "react";
+import config from "../config"
+import Swal from "sweetalert2";
+
 
 export default function Page() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signin = async () => {
+    try{ 
+      const payload = {
+        username: username,
+        password: password,
+      };
+      const res = await axios.post(
+        config.apiServer + "/api/user/signin",
+        payload
+      );
+      if (res.data.token !== undefined) {
+        localStorage.setItem(config.token, res.data.token);
+      } else {
+        Swal.fire({
+          title: "ตรวจสอบ username",
+          text: "username ไม่ถูกต้อง",
+          icon : "error",
+          confirmButtonText: "OK",
+        });
+      }
+
+    } catch (e: any) {
+      Swal.fire({
+        title: "error",
+        text: "e.message",
+        icon: "error"
+      })
+    }
+  }
   return (
     <>
       <div className="login-box">
@@ -15,12 +52,13 @@ export default function Page() {
           <div className="card-body">
             <p className="login-box-msg">Sign in to start your session</p>
 
-            <form action="../../index3.html" method="post">
+            <div>
               <div className="input-group mb-3">
                 <input
                   type="email"
                   className="form-control"
                   placeholder="Email"
+                  onChange={(e) => setUsername(e.target.value)}
                 />
                 <div className="input-group-append">
                   <div className="input-group-text">
@@ -33,6 +71,7 @@ export default function Page() {
                   type="password"
                   className="form-control"
                   placeholder="Password"
+                  onChange={(e) => setPassword(e.target.value)}
                 />
                 <div className="input-group-append">
                   <div className="input-group-text">
@@ -48,12 +87,16 @@ export default function Page() {
                   </div>
                 </div>
                 <div className="col-4">
-                  <button type="submit" className="btn btn-primary btn-block">
+                  <button 
+                      type="submit" 
+                      className="btn btn-primary btn-block"
+                      onClick={signin}>
+                      
                     Sign In
                   </button>
                 </div>
               </div>
-            </form>
+            </div>
 
             <div className="social-auth-links text-center mt-2 mb-3">
               <a href="#" className="btn btn-block btn-primary">
